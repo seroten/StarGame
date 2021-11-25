2d71b10c488d88d0e13821c3a10c71cf049eed86
Dmitry <seroten@mail.ru>
2021-11-25 13:15:05 +0300

    Добавил перемещение к нажатию

--- core/src/com/mygdx/game/screen/impl/MenuScreen.java
+++ core/src/com/mygdx/game/screen/impl/MenuScreen.java
@@ -7,10 +7,12 @@ import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.screen.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
+    private final float V_LEN = 10;
 
     private Texture img;
     private Vector2 touch;
     private Vector2 v;
+    private Vector2 pos;
 
     @Override
     public void show() {
@@ -18,14 +20,19 @@ public class MenuScreen extends BaseScreen {
         img = new Texture("badlogic.jpg");
         touch = new Vector2();
         v = new Vector2(1, 1);
+        pos = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
         batch.begin();
-        batch.draw(img, touch.x, touch.y);
+        batch.draw(img, pos.x, pos.y);
         batch.end();
+
+        if(touch.dst(pos) >V_LEN) {
+            pos.add(v);
+        } else pos.set(touch);
     }
 
     @Override
@@ -37,6 +44,7 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
         return super.touchDown(screenX, screenY, pointer, button);
     }
 }
