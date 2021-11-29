2016964988f7eff5a6449dde7cdffd4068eb5d51
Dmitry <seroten@mail.ru>
2021-11-29 16:12:26 +0300

    5

--- core/src/com/mygdx/game/screen/impl/MenuScreen.java
+++ core/src/com/mygdx/game/screen/impl/MenuScreen.java
@@ -4,38 +4,38 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.math.Rect;
 import com.mygdx.game.screen.BaseScreen;
+import com.mygdx.game.sprite.Logo;
 import com.mygdx.game.sprite.impl.Background;
 
 public class MenuScreen extends BaseScreen {
 
     private Texture img;
     private Texture bg;
-    private Vector2 pos;
 
     private Background background;
+    private Logo logo;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
         bg = new Texture("textures/bg.png");
-        pos = new Vector2();
         background = new Background(bg);
+        logo = new Logo(img);
     }
 
     @Override
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        logo.resize(worldBounds);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        batch.begin();
-        background.draw(batch);
-        batch.draw(img, pos.x, pos.y);
-        batch.end();
+        update(delta);
+        draw();
     }
 
     @Override
@@ -47,7 +47,18 @@ public class MenuScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        pos.set(touch);
-        return super.touchDown(touch, pointer, button);
+        logo.touchDown(touch, pointer,button);
+        return false;
+    }
+
+    private void update(float delta) {
+        logo.update(delta);
+    }
+
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        logo.draw(batch);
+        batch.end();
     }
 }
